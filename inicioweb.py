8ac33b7866193f849cc74a216eb03b0870ff30bf
marcoacsena <marcoacsena@gmail.com>
2025-01-28 08:35:24 -0300

    Copia dos aquivos do projeto anterior.

---
+++ inicioweb.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template_string
+
+import pandas as pd
+
+app = Flask(__name__)
+
+dfjson = pd.read_excel('datacleaning.xlsx')
+
+@app.route('/')
+
+def show_data():
+    table_html = dfjson.to_html(classes='table table-striped', index=False)
+    html = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Dados com Pandas</title>
+        <link rel="stylesheet" 
+              href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.5.2/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Dados do Arquivo JSON</h1>
+            {table_html}
+        </div>
+    </body>
+    </html>
+    """
+
+    return render_template_string(html)
+
+if __name__ == '__main__':
+    app.run(debug=True)
